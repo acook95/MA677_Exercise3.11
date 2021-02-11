b672a36fa6b72b778183af7cd579f77fd77f2d99
Anna Cook <annacook@bu.edu>
2021-02-10 23:05:56 -0500

    plot and critical values

--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 H0: p(cures headache) = 0.6
 Ha: p(cures headache) > 0.6
 
-both type 1 and type 2 errors < 0.05
+both type 1 (alpha) and type 2 (beta) errors < 0.05
+
+power = 1 - beta
+
+n = 100
+
+how many successes do we need to reject the null hypothesis with less than 5% type 1 and 2 errors?
--- experimenting w: code.R
+++ experimenting w: code.R
@@ -1,3 +1,6 @@
+
+### code from Haviland's demo
+
 options(digits = 3, scipen = 999)
 
 p <- dbinom(5,10,.3)
@@ -10,6 +13,7 @@ cumsum(x)
 
 y <- pbinom(0:10, 10, .3)
 y
+plot(y)
 
 inv <- qbinom(y, 10, .3)
 inv
@@ -25,3 +29,64 @@ samp1 <- as.data.frame(samp)
 dim(samp1)
 plot1 <- ggplot(samp1, aes(x=samp))
 plot1 + geom_histogram()
+
+
+
+### experimenting with code for 3.11
+options(digits = 3, scipen = 999)
+set.seed(1)
+n <- 100
+p0 <- 0.6
+p1 <- 0.8
+x <- pbinom(0:100, n, p0)
+x
+plot(x, type = "l")
+
+y <- pbinom(0:100, n, p1)
+plot(y, type = "l")
+
+plot(x, type = "l")
+lines(y, type = "l")
+
+z <- dbinom(0:100, n, p0)
+z
+plot(z)
+
+for (i in 0:n) {
+  alpha <- 1 - pbinom(i, n, p0)
+  if (alpha < 0.05) break
+}
+m0 <- i
+m0
+
+for (i in 0:n) {
+  beta <- pbinom(i, n, p1)
+  if (beta > 0.05) break
+}
+m1 <- i
+m1
+
+
+plot(x, type = "l")
+lines(y, type = "l")
+segments(60, 0.05, 80, 0.05)
+segments(60, 0.05, 60, 0.95)
+segments(60, 0.95, 80, 0.95)
+segments(80, 0.95, 80, 0.05)
+
+
+
+for (i in seq(from = 0.4, to = 1, by = 0.001)) {
+  pwr <- pbinom(m0, size = 100, prob = i)
+}
+plot(pwr)
+
+pwr <- pbinom(m0, 100, prob = seq(from = 0.4, to = 1, by = 0.01))
+plot(pwr)
+
+
+pwr1 <- cumsum(dbinom(m0, 100, prob = seq(from = 0.4, to = 1, by = 0.01)))
+plot(pwr1, type = "l")
+
+pwr2 <- cumsum(dbinom(m1, 100, prob = seq(from = 0.4, to = 1, by = 0.1)))
+plot(pwr1, type = "l")
